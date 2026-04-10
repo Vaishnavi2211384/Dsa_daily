321b963ff734cf66460839f00ad114367e74a9c8
Vaishnavi2211384 <trivedivaishnavi43@gmail.com>
2026-04-10 22:48:29 +0530

    Updated code

---
+++ day1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    for(int i=1;i<=5;i++){
+        int n=i*i;
+        cout<<i<<"-"<<n<<endl;
+    }
+}
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write your code here
+    int a=8;
+    int b=4;
+    cout<<a+b<<endl<<a-b<<endl<<a/b<<endl<<a*b;
+    
+}
+//codechef problem done with all mcq's and questions which comes under free version
