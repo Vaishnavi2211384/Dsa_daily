4647199890671931aa7056d9c8da6286266a5cab
Vaishnavi2211384 <trivedivaishnavi43@gmail.com>
2026-04-24 23:44:29 +0530

    Day 15: solve problems

---
+++ day15.cpp
@@ -0,0 +1,20 @@
+//Even Odd Game
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"Friend";
+    }
+    else{
+        cout<<"You";
+    }
+
+    return 0;
+}
+
+//
