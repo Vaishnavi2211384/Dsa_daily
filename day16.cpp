765eb762a64f401fd6a726b8e6975ba0e82bbb33
Vaishnavi2211384 <trivedivaishnavi43@gmail.com>
2026-04-25 22:27:56 +0530

    Day 16: solve problems

---
+++ day16.cpp
@@ -0,0 +1,21 @@
+//C++ | A Speed Breaker
+
+// User function Template for C++
+class Solution {
+  public:
+    void speedBreaker(double a, int b) {
+        // code here
+          cout<<fixed<<setprecision(b)<<a<<endl;
+        
+    }
+};
+
+//Print GeeksForGeeks with quotes
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"\"GeeksForGeeks\"";
+    return 0;
+}
